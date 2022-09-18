74722164bb97b8caf16e47beea673710ba8def38
[Wyatt Jenkins s3770872] <s3770872@student.rmit.edu.au>
2022-09-18 22:11:31 +1000

    Added base column config for entity attributes

--- src/main/java/com/rmit/septt16msbookingsystem/model/DoctorAvailability.java
+++ src/main/java/com/rmit/septt16msbookingsystem/model/DoctorAvailability.java
@@ -2,6 +2,7 @@ package com.rmit.septt16msbookingsystem.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.Date;
@@ -11,9 +12,16 @@ import java.util.Date;
 public class DoctorAvailability {
 
     @Id
+    @Column(name="", nullable=false, unique=true)
     private String doctorId;
+
+    @Column(name="", nullable=false, unique=true)
     private String doctorName;
+
+    @Column(name="", nullable=false, unique=true)
     private Date doctorAvailabilityStartTime;
+
+    @Column(name="", nullable=false, unique=true)
     private Date doctorAvailabilityEndTime;
 
 }
