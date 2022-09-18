0874543adfcff5f6cb3a9febee46f78cceb8ef74
[Wyatt Jenkins s3770872] <s3770872@student.rmit.edu.au>
2022-09-18 23:44:10 +1000

    Commented tests for development purposes

--- src/test/java/com/rmit/septt16msbookingsystem/SeptT16MsBookingSystemApplicationTests.java
+++ src/test/java/com/rmit/septt16msbookingsystem/SeptT16MsBookingSystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SeptT16MsBookingSystemApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
