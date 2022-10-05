58bd0019eb7b3d96c9ee827bfd8d3d8c578e578b
[Wyatt Jenkins s3770872] <s3770872@student.rmit.edu.au>
2022-10-05 14:14:05 +1100

    Replaced wildcard imports with singletons

--- src/main/java/com/rmit/septt16msbookingsystem/service/BookingService.java
+++ src/main/java/com/rmit/septt16msbookingsystem/service/BookingService.java
@@ -11,7 +11,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.*;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Slf4j
